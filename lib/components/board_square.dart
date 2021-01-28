deb93db73afa6986c0e6b57747e206e78328f052
Yuri Azevedo <yuriazevedo11@gmail.com>
2021-01-27 22:09:15 -0300

    feat: Add coordinates to BoardSquare component

--- lib/components/board_square.dart
+++ lib/components/board_square.dart
@@ -12,14 +12,47 @@ class BoardSquare extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double size = MediaQuery.of(context).size.width / 8;
-
     int rowOffset = rowIndex % 2 == 0 ? 1 : 0;
-    Color squareColor = (columnIndex + rowOffset) % 2 == 1 ? WHITE : BLACK;
+    bool isWhite = (columnIndex + rowOffset) % 2 == 1;
+
+    Color squareColor = isWhite ? WHITE : BLACK;
+    Color textColor = isWhite ? BLACK : WHITE;
 
     return Container(
       height: size,
       width: size,
       color: squareColor,
+      child: Padding(
+        padding: EdgeInsets.all(4.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Text(
+                  (8 - columnIndex).toString(),
+                  style: TextStyle(
+                    color: textColor.withAlpha(rowIndex == 0 ? 255 : 0),
+                    fontSize: 12.0,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  String.fromCharCode('a'.codeUnits[0] + rowIndex),
+                  style: TextStyle(
+                    color: textColor.withAlpha(columnIndex == 7 ? 255 : 0),
+                    fontSize: 12.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
