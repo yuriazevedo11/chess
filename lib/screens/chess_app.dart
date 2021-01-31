39a24c02690f2abd58c843338190598c863a0acf
Yuri Azevedo <yuriazevedo11@gmail.com>
2021-01-31 16:22:20 -0300

    feat: Convert ChessApp screen to StatefulWidget

--- lib/screens/chess_app.dart
+++ lib/screens/chess_app.dart
@@ -1,14 +1,25 @@
 import 'package:chess/components/board.dart';
 import 'package:chess/models/chess.dart';
+import 'package:chess/models/piece.dart';
 import 'package:flutter/material.dart';
 
-class ChessApp extends StatelessWidget {
-  final Chess chess = Chess();
+class ChessApp extends StatefulWidget {
+  @override
+  _ChessAppState createState() => _ChessAppState();
+}
+
+class _ChessAppState extends State<ChessApp> {
+  static final Chess chess = Chess();
+  List<List<Piece>> board;
+
+  @override
+  initState() {
+    super.initState();
+    board = chess.board();
+  }
 
   @override
   Widget build(BuildContext context) {
-    var board = chess.board();
-
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.grey[900],
